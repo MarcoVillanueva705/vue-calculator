957200f1f0ba1a5ce02a5157ecc4c72758890341
marco villanueva <villanuevamarco98@gmail.com>
2020-04-10 15:38:11 -0600

    added calculator component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Calulator />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,6 +8,16 @@
     <router-view/>
   </div>
 </template>
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+</script>
 
 <style>
 #app {
